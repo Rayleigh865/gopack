8850e02fce6b318ecc7da76e26c6a13925622a12
Rayleigh865 <rayleigh865@gmail.com>
2020-05-10 00:02:28 +0900

    add example

---
+++ example/test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/Rayleigh865/gopack"
+	"log"
+)
+
+func main() {
+	p := gopack.NewPacker()
+
+	p.AddBin(gopack.NewBin("Small Bin", 100, 100))
+
+	p.AddItem(gopack.NewItem("Item 1", 2, 2))
+	p.AddItem(gopack.NewItem("Item 2", 10, 5))
+	p.AddItem(gopack.NewItem("Item 3", 20, 10))
+	p.AddItem(gopack.NewItem("Item 4", 5, 5))
+
+	if err := p.Pack(); err != nil {
+		log.Fatal(err)
+	}
+
+	gopack.Display_packed(p.Bins)
+}
